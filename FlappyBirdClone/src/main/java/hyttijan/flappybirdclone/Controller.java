4b031c1367ad8ce16bb457f6c1b48207e61c99a1
Janne <hyttijan@cs.helsinki.fi>
2015-10-28 11:01:31 +0200

    Projekti aloitettu

---
+++ FlappyBirdClone/src/main/java/hyttijan/flappybirdclone/Controller.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hyttijan.flappybirdclone;
+
+/**
+ *
+ * @author janne
+ */
+public class Controller {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
